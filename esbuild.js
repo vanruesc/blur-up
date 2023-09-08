ed40092b52125ed0fe7fe67ca0608e9dce83accc
Raoul v. R <vanruesc@users.noreply.github.com>
2023-09-08 13:15:35 +0200

    Update esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -1,45 +1,37 @@
-import { createRequire } from "module";
+import pkg from "./package.json" assert { type: "json" };
 import esbuild from "esbuild";
 
-const require = createRequire(import.meta.url);
-const pkg = require("./package");
-const date = (new Date()).toDateString();
-const external = Object.keys(pkg.dependencies).concat(["path"]);
-
+const date = new Date();
 const banner = `/**
- * ${pkg.name} v${pkg.version} build ${date}
+ * ${pkg.name} v${pkg.version} build ${date.toDateString()}
  * ${pkg.homepage}
- * Copyright ${date.slice(-4)} ${pkg.author.name}
+ * Copyright 2018 ${pkg.author.name}
  * @license ${pkg.license}
  */`;
 
 const shebang = "#!/usr/bin/env node";
+const external = Object.keys(pkg.dependencies).concat(["path"]);
 
-const common = {
+await esbuild.build({
+	entryPoints: ["src/index.ts"],
+	outfile: "dist/index.js",
+	banner: { js: banner },
 	platform: "node",
 	logLevel: "info",
+	format: "esm",
 	bundle: true,
 	minify: true,
 	external
-};
+});
 
-await esbuild.build(Object.assign({
-	entryPoints: ["src/index.ts"],
-	outfile: `dist/${pkg.name}.js`,
-	banner: { js: banner },
-	format: "esm"
-}, common)).catch(() => process.exit(1));
-
-await esbuild.build(Object.assign({
-	entryPoints: ["src/index.ts"],
-	outfile: `dist/${pkg.name}.cjs`,
-	banner: { js: banner },
-	format: "cjs"
-}, common)).catch(() => process.exit(1));
-
-await esbuild.build(Object.assign({
+await esbuild.build({
 	entryPoints: ["src/cli.ts"],
-	outfile: `dist/cli.cjs`,
+	outfile: "dist/cli.js",
 	banner: { js: shebang },
-	format: "cjs"
-}, common)).catch(() => process.exit(1));
+	platform: "node",
+	logLevel: "info",
+	format: "esm",
+	bundle: true,
+	minify: true,
+	external
+});
