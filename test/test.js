522e8617dacf554ca9260f26391bc1e5d1e20166
Raoul v. R. <vanruesc@outlook.de>
2025-03-28 19:59:50 +0100

    Update tests

--- test/test.js
+++ test/test.js
@@ -40,12 +40,44 @@ test("honors options", async(t) => {
 
 });
 
-test("falls back to the input file name", async(t) => {
+test("can handle transparent images", async(t) => {
 
-	return blurUp("test/images/img.jpg", "test/generated", {
+	return blurUp("test/images/transparent.png", "test/generated/c", {
 		stdDeviationX: 30,
 		width: 20
-	}).then(async() => access("test/generated/img.svg", constants.F_OK).then((error) => {
+	}).then(async() => {
+
+		const actual = await readFile("test/generated/c/transparent.svg", "utf8");
+		const expected = await readFile("test/expected/c/transparent.svg", "utf8");
+
+		t.is(actual, expected);
+
+	});
+
+});
+
+test("does not blur edges of opaque PNG images", async(t) => {
+
+	return blurUp("test/images/opaque.png", "test/generated/d", {
+		stdDeviationX: 30,
+		width: 20
+	}).then(async() => {
+
+		const actual = await readFile("test/generated/d/opaque.svg", "utf8");
+		const expected = await readFile("test/expected/d/opaque.svg", "utf8");
+
+		t.is(actual, expected);
+
+	});
+
+});
+
+test("falls back to the input file name", async(t) => {
+
+	return blurUp("test/images/img.jpg", "test/images", {
+		stdDeviationX: 30,
+		width: 20
+	}).then(async() => access("test/images/img.svg", constants.F_OK).then((error) => {
 
 		t.is(error, undefined);
 
