84080515286ed8abb6758be80d2aeef7a48dccb4
Raoul v. R. <vanruesc@outlook.de>
2026-06-26 00:00:43 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { blurUp as default } from "./blurUp.js";
 export { blurUp } from "./blurUp.js";
-export { BlurUpConfig } from "./BlurUpConfig.js";
-export { BlurUpOptions } from "./BlurUpOptions.js";
+export type { BlurUpConfig } from "./BlurUpConfig.js";
+export type { BlurUpOptions } from "./BlurUpOptions.js";
